62238298a13551a91ffc6826df68ebf626c0e4dd
Tornike ZARDIASHVILI <tornike.zar@gmail.com>
2024-10-31 20:46:01 +0100

    added netlify configuration for react

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
